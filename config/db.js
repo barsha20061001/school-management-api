4c916b641dafe920f6fb24625111c0b0a8b77486
barsha20061001 <barshadgp212@gmail.com>
2026-05-21 00:48:17 +0530

    fix db connection timeout

--- config/db.js
+++ config/db.js
@@ -3,19 +3,23 @@ require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 10000,
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed:", err.message);
-    return;
+    console.error("Database connection failed:", err.message);
+  } else {
+    console.log("MySQL database connected successfully");
   }
-
-  console.log("MySQL database connected successfully");
 });
 
 module.exports = db;
+
+
+
 
